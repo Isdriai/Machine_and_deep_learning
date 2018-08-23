9c0f3a59a615a8b06d1311f56e3b2752047214b0
picot <picot.rodolphe@gmail.com>
2018-08-23 17:25:40 +0200

    in process

--- intro_marchine_learning/Partie 2 - Regression/Regression Linéaire/lineaire_multiple.py
+++ intro_marchine_learning/Partie 2 - Regression/Regression Linéaire/lineaire_multiple.py
@@ -27,28 +27,27 @@ imputer.fit(x[:,1:3])
 
 x[:, 1:3] = imputer.transform(x[:,1:3])
 
+'''
+
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 labelEncode_country = LabelEncoder()
-x[:,0] = labelEncode_country.fit_transform(x[:,0])
+x[:,3] = labelEncode_country.fit_transform(x[:,3])
 
-oneHotEncoder = OneHotEncoder(categorical_features=[0])
+oneHotEncoder = OneHotEncoder(categorical_features=[3])
 
 x = oneHotEncoder.fit_transform(x).toarray()
 
+x = x[:, 1:] 
 
+'''
+la variable dependante est numérique
 labelEncode_y = LabelEncoder()
 y = labelEncode_y.fit_transform(y)
-
+'''
 
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
-from sklearn.preprocessing import StandardScaler
-
-sc = StandardScaler()
-x_train = sc.fit_transform(x_train)
-x_test = sc.transform(x_test)
-'''
 
